8cda327f9d53b47415eaf4956dcb8c18a10283a0
LengHuaa <33490977+LengHuaa@users.noreply.github.com>
2018-06-01 08:55:49 +0100

    Create node app.js

---
+++ node app.js
@@ -0,0 +1,7 @@
+{
+    "token": "NDQxMzM1MzExMzI3NTU5NzAx.DfIC8Q.TxO7x5WnJrLV4jCEYMsxvsm3wE8",
+    "prefix": "*t",
+    "owner": ["339081678586904577"],
+    "playingStatus": "*t (language name) any text here. Ask Lisa for help.",
+    "tsChannelsEnabled": true
+}
